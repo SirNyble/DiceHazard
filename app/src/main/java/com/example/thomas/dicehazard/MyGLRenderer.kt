6dbdb692b79e5332c3efd341d59cc45472fb9504
SirNyble <thomasrhys14@gmail.com>
2018-04-01 20:27:41 -0300

    Adding Triangle object to be rendered in MyGLRenderer

--- app/src/main/java/com/example/thomas/dicehazard/MyGLRenderer.kt
+++ app/src/main/java/com/example/thomas/dicehazard/MyGLRenderer.kt
@@ -7,16 +7,37 @@ import android.opengl.GLSurfaceView
 
 
 class MyGLRenderer : GLSurfaceView.Renderer {
+    private var mTriangle: Triangle? = null
+
     override fun onSurfaceCreated(gl: GL10?, config: javax.microedition.khronos.egl.EGLConfig?) {
         GLES20.glClearColor(0.5f, 0.0f, 0.0f, 1.0f)
+
+        mTriangle = Triangle()
     }
 
     override fun onDrawFrame(unused: GL10) {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+
+        mTriangle?.draw()
     }
 
     override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
         GLES20.glViewport(0, 0, width, height)
+    }
+
+    companion object {
+        fun loadShader(type: Int, shaderCode: String): Int {
+
+            // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+            // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+            val shader = GLES20.glCreateShader(type)
+
+            // add the source code to the shader and compile it
+            GLES20.glShaderSource(shader, shaderCode)
+            GLES20.glCompileShader(shader)
+
+            return shader
+        }
     }
 }

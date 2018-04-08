9106fa1b33d732e6816099dd9890ab7b688f3e8a
SirNyble <thomasrhys14@gmail.com>
2018-04-08 17:38:07 -0300

    Enable depth buffer and rotate camera

--- app/src/main/java/com/example/thomas/dicehazard/MyGLRenderer.kt
+++ app/src/main/java/com/example/thomas/dicehazard/MyGLRenderer.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.opengl.Matrix
+import java.lang.Math.cos
+import java.lang.Math.sin
 import javax.microedition.khronos.opengles.GL10
 
 
@@ -18,25 +20,38 @@ class MyGLRenderer(context: Context) : GLSurfaceView.Renderer {
   private val mProjectionMatrix = FloatArray(16)
   private val mViewMatrix = FloatArray(16)
 
+  private var mStartTime: Long = 0
+
   init {
     mContext = context
+    mStartTime = System.currentTimeMillis()
   }
 
   override fun onSurfaceCreated(gl: GL10?, config: javax.microedition.khronos.egl.EGLConfig?) {
     GLES20.glClearColor(0.2f, 0.3f, 0.3f, 1.0f)
-
+    GLES20.glEnable(GLES20.GL_DEPTH_TEST)
     mTriangle = Triangle()
 
     val inputStream = mContext?.assets?.open("astroBoy_walk_Max.dae")
-    mModel = Model(inputStream!!)
+    mModel = Model(mContext!!, inputStream!!)
   }
 
   override fun onDrawFrame(unused: GL10) {
     // Redraw background color
-    GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+    GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT or GLES20.GL_DEPTH_BUFFER_BIT )
+
+
+    val tEnd = System.currentTimeMillis()
+    val tDelta = tEnd - mStartTime
+    val elapsedSeconds = tDelta / 1000.0
+
+
+    val radius = 20.0f
+    val camX = sin(elapsedSeconds) * radius;
+    val camZ = cos(elapsedSeconds) * radius;
 
     // Set the camera position (View matrix)
-    Matrix.setLookAtM(mViewMatrix, 0, 0.0f, 15.0f, 15.0f,
+    Matrix.setLookAtM(mViewMatrix, 0, camX.toFloat(), 10.0f, camZ.toFloat(),
         0f, 0f, 0f, 0f, 1.0f, 0.0f)
 
     // Calculate the projection and view transformation
